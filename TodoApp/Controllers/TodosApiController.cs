383c65e3c29e0edcf01b7cb6a91fd805457ccb03
GaMInST <gaminst2@gmail.com>
2025-05-07 13:02:39 +0300

    Swagger

---
+++ TodoApp/Controllers/TodosApiController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodosApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodosApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodosApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Todo>>> GetTodos()
+        {
+            return await _context.Todos.ToListAsync();
+        }
+
+        // GET: api/TodosApi/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Todo>> GetTodo(Guid id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null)
+                return NotFound();
+
+            return todo;
+        }
+
+        // POST: api/TodosApi
+        [HttpPost]
+        public async Task<ActionResult<Todo>> CreateTodo(Todo todo)
+        {
+            todo.Id = Guid.NewGuid();
+            todo.CreatedDate = DateTime.Now;
+            todo.LastModifiedDate = DateTime.Now;
+
+            _context.Todos.Add(todo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodo), new { id = todo.Id }, todo);
+        }
+
+        // PUT: api/TodosApi/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTodo(Guid id, Todo todo)
+        {
+            if (id != todo.Id)
+                return BadRequest();
+
+            todo.LastModifiedDate = DateTime.Now;
+            _context.Entry(todo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/TodosApi/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodo(Guid id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null)
+                return NotFound();
+
+            _context.Todos.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoExists(Guid id)
+        {
+            return _context.Todos.Any(t => t.Id == id);
+        }
+    }
+}
